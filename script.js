4ecc73b83ef690d0b088e0c16cdd0649087ab7d2
xaviguasch <xaviguasch@gmail.com>
2020-06-17 18:37:02 +0200

    added javascript logic and basic styling

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-const a = 'thee'
+const input = document.getElementById('temperatureInput')
+const tempSelector = document.getElementById('tempSelect')
+const form = document.getElementById('main-form')
+const result = document.getElementById('result')
+
+let inputValue = 0
+let selectedTemp = 'fahrenheit'
+
+function displayResult(res) {
+  result.innerText = res
+}
+
+function convertToF(c) {
+  const res = c * (9 / 5) + 32
+
+  displayResult(res)
+}
+
+function convertToC(f) {
+  const res = ((f - 32) * 5) / 9
+  displayResult(res)
+}
+
+tempSelector.addEventListener('change', function (e) {
+  selectedTemp = e.target.value
+})
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+
+  inputValue = input.value
+
+  if (selectedTemp === 'fahrenheit') {
+    console.log(convertToC(inputValue))
+  }
+
+  if (selectedTemp === 'celsius') {
+    console.log(convertToF(inputValue))
+  }
+})
